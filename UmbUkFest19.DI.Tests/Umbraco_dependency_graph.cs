4262e6e5ec26bd6f9a951760b1a259899845b991
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2019-10-15 23:58:54 +0200

    Test running with "nightly" of "fix tests" PR

---
+++ UmbUkFest19.DI.Tests/Umbraco_dependency_graph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Umbraco.Tests.TestHelpers;
+using Umbraco.Tests.Testing;
+
+[SetUpFixture]
+public class Setup_UmbracoTest_Discovery
+{
+    [OneTimeSetUp]
+    public static void Register()
+    {
+        TestOptionAttributeBase.ScanAssemblies.Add(typeof(Setup_UmbracoTest_Discovery).Assembly.FullName);
+    }
+}
+
+namespace UmbUkFest19.DI.Tests
+{
+    [TestFixture]
+    public class Umbraco_Dependency_Graph : BaseWebTest
+    {
+        [Test]
+        public void As_Ascii()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
